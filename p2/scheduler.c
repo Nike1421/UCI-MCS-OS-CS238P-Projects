6997574957e30d199b6796663c7b3f031de90289
puneetn <puneeetnagar@gmail.com>
2023-10-22 18:32:07 -0700

    schedule_yield implemented

--- p2/scheduler.c
+++ p2/scheduler.c
@@ -177,7 +177,13 @@ void schedule(void){
  */
 
 void scheduler_yield(void){
-
+    if(setjmp(state.current_thread->ctx)){
+        return;
+    }
+    else{
+        state.current_thread->thread_status=STATUS_SLEEPING;
+        longjmp(state.ctx, 0);
+    }
 }
 
 
